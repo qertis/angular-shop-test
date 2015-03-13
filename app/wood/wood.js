77dcaa126aba423dbfdf61fabb95a76e81070a5c
qertis <iavtoritet@gmail.com>
2015-03-13 18:33:59 +0300

    тестовый поиск

--- app/wood/wood.js
+++ app/wood/wood.js
@@ -9,10 +9,25 @@ angular.module('myApp.wood',
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider.when('/wood', {
 			templateUrl: 'wood/wood.html',
-			controller: 'View1Ctrl'
+			controller: 'WoodCtrl'
 		});
 	}])
 
-	.controller('View1Ctrl', [function () {
+	.controller('WoodCtrl', ['$scope', function ($scope) {
+		$scope.materials =
+			[
+				{ label: 'дуб', value: 1 },
+				{ label: 'бук', value: 2 },
+				{ label: 'ясень', value: 3 }
+			];
+		$scope.colors = [
+			{ label: 'orange', value: 1 },
+			{ label: 'purple', value: 2 },
+			{ label: 'yellow', value: 3 }
+		]
 
+		$scope.materialSelected = $scope.materials[0]
+		$scope.colorSelected = $scope.colors[0]
+		$scope.price = 0;
+		
 	}]);
--- app/wood/wood_test.js
+++ app/wood/wood_test.js
@@ -8,8 +8,8 @@ describe('myApp.wood module', function() {
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var WoodCtrl = $controller('WoodCtrl');
+      expect(WoodCtrl).toBeDefined();
     }));
 
   });
